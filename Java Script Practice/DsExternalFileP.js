7cccf869aedd77be83e4819fe47e7bad53eae3ef
Zachariah-W <zachwangs@gmail.com>
2023-04-07 21:51:22 -0700

    Practice Js Output

--- Java Script Practice/DsExternalFileP.js
+++ Java Script Practice/DsExternalFileP.js
@@ -1,3 +1,16 @@
 function exfilefunction1(){
      document.getElementById('exfp1').innerHTML = 'Practice for Js External file';
 }
+document.getElementById('pfhtml').innerHTML = 3 + 7;
+function alertFunction(){
+     alert("the text will change");
+     document.getElementById('pfalert').innerHTML = "I smell demon";
+     console.log("The function work")
+}
+function statementFunction(){
+     let x, y, z;
+     y=8;
+     x=4;
+     z=y+x;
+     document.getElementById('pfstatement').innerHTML = "The value for statement practice " + z + ".";
+}
